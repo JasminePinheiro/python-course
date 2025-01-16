fc30632b944364ac9db9ead8403daa12c13f1c9a
Jasmine Pinheiro <jasminepinheiro14@gmail.com>
2025-01-15 22:00:33 -0300

    feat: orientação a objeto (herança e polimorfismo)

--- data_science_academy/fundamentos_analise_de_dados/classe.py
+++ data_science_academy/fundamentos_analise_de_dados/classe.py
@@ -6,3 +6,97 @@ class Algoritmo():
 algo = Algoritmo(tipo_algo= 'Teste')
 
 print(algo.tipo)
+
+# Os métodos sempre inclui o parâmetro self como o primeiro argumento que é usando para se referir ao objeto atual da classe 
+# O método __init__ em Python é um método especial usado para inicializar um objeto quando ele é criado
+class Circulo():
+    
+    pi = 3.14
+    
+    # Quando um objeto desta classe for criado, este método será executado e o valor default do raio será 5
+    def __init__(self, raio = 5):
+       self.raio = raio
+    
+    # calcula a área
+    def area(self):
+        return (self.raio * self.raio) + Circulo.pi
+    
+   # gerar um novo raio
+    def setRaio(self, novo_raio):
+        self.raio = novo_raio
+        
+    # obter o raio do circulo    
+    def getRaio(self):
+        return self.raio
+    
+
+# criando o objeto circ, uma instâmcia da classe Circulo()
+circ = Circulo()
+
+print('A área igual a:', circ.area())
+
+# Exemplo de polimorfismo -> Refere a habilidade de um objeto responder de diferentes forma a uma mesam mensagem
+# Polimorfismo - Permite que diferentes classes tenham métodos com o mesmo nome, mas com implementações diferentes. 
+# SuperClasse
+# Ela permite que uma classe (subclasse) herde atributos e métodos de outra classe (superclasse ou classe base). 
+
+# Superclasse
+class Veiculo:
+    
+    def __init__(self, marca, modulo):
+       self.marca = marca
+       self.modulo = modulo
+       
+    def acelerar(self):
+        pass
+    
+    def frear(self):
+        pass
+    
+
+# Subclasse
+class Carro(Veiculo):
+    
+    def acelerar(self):
+        print("O carro está acelerando")
+        
+    def frear(self):
+        print("O carro está freando")
+  
+# Subclasse  
+class Moto(Veiculo):
+    
+    def acelerar(self):
+        print("O moto está acelerando")
+        
+    def frear(self):
+        print("O moto está freando")
+        
+# Subclasse  
+class Aviao(Veiculo):
+    
+    def acelerar(self):
+        print("O aviao está acelerando")
+        
+    def frear(self):
+        print("O aviao está freando")
+    
+    def decolar(self):
+        print("O aviao está decolando")
+        
+        
+
+# Cria objetos
+
+lista_veiculos = [Carro("kkk", "ttt"), Moto("www", "ggg"), Aviao("ttt", "bla")]
+
+for item in lista_veiculos:
+    
+    item.acelerar()
+    
+    item.frear()
+    
+    if isinstance(item, Aviao):
+        item.decolar()
+    
+    print('---')
